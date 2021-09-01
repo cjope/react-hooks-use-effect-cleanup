c762a2dbe7b335114f8d4aad5f157db95f3cc2dc
Craig <craigjope@gmail.com>
2021-08-31 21:04:40 -0600

    commit tree 10d001d7d7fed4fc3596f217f7e9f000009a5394

--- src/App.js
+++ src/App.js
@@ -5,10 +5,13 @@ import Clock from "./Clock";
 function App() {
   const [showClock, setShowClock] = useState(true);
 
+
+  const clockButton = (showClock === true ? "Hide Clock" : "Show Clock") 
+
   return (
     <div>
+      <button onClick={() => setShowClock(!showClock)}>{clockButton}</button>
       {showClock ? <Clock /> : null}
-      <button onClick={() => setShowClock(!showClock)}>Toggle Clock</button>
     </div>
   );
 }
